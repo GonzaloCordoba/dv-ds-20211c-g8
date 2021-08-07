9074d740862f42bd92d50c982eb43c72d891221a
gonzalo cordoba <gonzaloc@onesolutions.com.ar>
2021-08-07 18:31:23 -0300

    Nuevo proyecto

---
+++ src/main/java/ar/edu/davinci/dvds20211cg8/DvDs20211cG8Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cg8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20211cG8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20211cG8Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20211cg8/DvDs20211cG8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20211cg8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20211cG8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
